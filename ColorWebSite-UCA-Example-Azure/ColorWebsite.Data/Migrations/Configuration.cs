1c8581bf71064d87a87e1df5b9f68ad462e2a4b1
JJ <jjorozcodev@gmail.com>
2019-11-15 09:39:27 -0600

    Actualización de Bootstrap y personalización de diseño.

--- ColorWebSite-UCA-Example-Azure/ColorWebsite.Data/Migrations/Configuration.cs
+++ ColorWebSite-UCA-Example-Azure/ColorWebsite.Data/Migrations/Configuration.cs
@@ -1,11 +1,9 @@
 namespace ColorWebsite.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using Entities;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ColorWebsite.Data.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
     {
         public Configuration()
         {
@@ -14,15 +12,15 @@ namespace ColorWebsite.Data.Migrations
             ContextKey = "ColorWebsite.Data.DataContext";
         }
 
-        protected override void Seed(ColorWebsite.Data.DataContext context)
+        protected override void Seed(DataContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             context.Colors.AddOrUpdate(
               p => p.Name,
-              new DemoColor { Name = "Orange" },
-              new DemoColor { Name = "Blue" },
-              new DemoColor { Name = "DarkGray" }
+              new DemoColor { Name = "Purple" },
+              new DemoColor { Name = "Teal" },
+              new DemoColor { Name = "LightSlateGray" }
             );
             
         }
